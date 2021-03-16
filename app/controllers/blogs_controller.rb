d835f00f5b516b2a785209998c58b9d85bd8eecf
yuki ohno <jeanadler33@gmail.com>
2021-03-16 05:07:51 +0000

    修正: def ~ endとdef ~ endの間の空行挿入を統一

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,15 @@
 class BlogsController < ApplicationController
+
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @blogs = Blog.all
   end
+
   def new
     @blog = Blog.new
   end
+
   def create
     @blog = Blog.new(blog_params)
     if params[:back]
@@ -47,7 +51,9 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
+
 end
